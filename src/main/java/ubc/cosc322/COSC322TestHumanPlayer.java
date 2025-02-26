4945abc75dd8e265d9d738ad7c93f93ff2c56a31
nayakashish <a.samuel.nayak@gmail.com>
2025-02-26 12:23:13 -0800

    Added HumanPlayer Test files

---
+++ src/main/java/ubc/cosc322/COSC322TestHumanPlayer.java
@@ -0,0 +1,111 @@
+
+package ubc.cosc322;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+import ygraph.ai.smartfox.games.BaseGameGUI;
+import ygraph.ai.smartfox.games.GameClient;
+import ygraph.ai.smartfox.games.GamePlayer;
+import ygraph.ai.smartfox.games.amazons.HumanPlayer;
+
+/**
+ * An example illustrating how to implement a GamePlayer
+ * @author Yong Gao (yong.gao@ubc.ca)
+ * Jan 5, 2021
+ *
+ */
+public class COSC322TestHumanPlayer extends GamePlayer{
+
+    private GameClient gameClient = null; 
+    private BaseGameGUI gamegui = null;
+	
+    private String userName = null;
+    private String passwd = null;
+ 
+	
+    /**
+     * The main method
+     * @param args for name and passwd (current, any string would work)
+     */
+    public static void main(String[] args) {				 
+    	// COSC322Test player = new COSC322Test(args[0], args[1]);
+		// COSC322TestCopy player = new COSC322TestCopy("cosc322asdf", "cosc322asdf");
+
+		GamePlayer player = new HumanPlayer();
+
+		// HumanPlayer player = new HumanPlayer();
+		// player.Go();
+		// player.
+		
+		// System.out.println("tkjahlkdsjhsdf");
+    	if(player.getGameGUI() == null) {
+    		player.Go();
+    	}
+    	else {
+    		BaseGameGUI.sys_setup();
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                	player.Go();
+                }
+            });
+    	}
+	
+    }
+	
+    /**
+     * Any name and passwd 
+     * @param userName
+      * @param passwd
+     */
+    public COSC322TestHumanPlayer(String userName, String passwd) {
+    	this.userName = userName;
+    	this.passwd = passwd;
+    	
+    	//To make a GUI-based player, create an instance of BaseGameGUI
+    	//and implement the method getGameGUI() accordingly
+    	this.gamegui = new BaseGameGUI(this);
+
+    }
+ 
+
+
+    @Override
+    public void onLogin() {
+    }
+
+    @Override
+    public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
+    	//This method will be called by the GameClient when it receives a game-related message
+    	//from the server.
+    	return true;   	
+    }
+    
+    
+    @Override
+    public String userName() {
+    	return userName;
+    }
+
+	@Override
+	public GameClient getGameClient() {
+		// TODO Auto-generated method stub
+		return this.gameClient;
+	}
+
+	@Override
+	public BaseGameGUI getGameGUI() {
+		// TODO Auto-generated method stub
+
+		return this.gamegui;
+	}
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+    	gameClient = new GameClient(userName, passwd, this);			
+	}
+
+ 
+}//end of class
